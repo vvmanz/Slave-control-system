4d292fd865e7dc73079aa6cd0618b8f3f020d729
vvmanz <vvmanz23@gmail.com>
2023-04-19 22:11:26 +0700

    Update stm32f1xx_it.h

--- Core/Inc/stm32f1xx_it.h
+++ Core/Inc/stm32f1xx_it.h
@@ -59,6 +59,8 @@ void DMA1_Channel1_IRQHandler(void);
 void TIM1_UP_IRQHandler(void);
 void TIM2_IRQHandler(void);
 void TIM3_IRQHandler(void);
+void TIM4_IRQHandler(void);
+void SPI2_IRQHandler(void);
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
